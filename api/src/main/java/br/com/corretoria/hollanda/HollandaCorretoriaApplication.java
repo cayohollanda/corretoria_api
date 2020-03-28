aebeb335cf17ee8d9acaffd8a3bf4b64dc68f868
Cayo Hollanda <cayohollanda@hotmail.com>
2020-03-28 17:53:41 -0300

    adicionando base do projeto

---
+++ api/src/main/java/br/com/corretoria/hollanda/HollandaCorretoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.corretoria.hollanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollandaCorretoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HollandaCorretoriaApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/corretoria/hollanda/HollandaCorretoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.corretoria.hollanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollandaCorretoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
